490bcb55f5b12047c97f0c372e7c47f4c5f0adab
porchy6666 <denis1997.da@gmail.com>
2026-06-13 00:33:38 +0300

    URBN first

---
+++ index.js
@@ -0,0 +1,321 @@
+const masters = [
+    {
+        id: "bogdan",
+        name: "Богдан",
+        works: [
+            "../Tatto.Pet/Богдан/photo_1.jpg",
+            "../Tatto.Pet/Богдан/photo_2.jpg",
+            "../Tatto.Pet/Богдан/photo_3.jpg",
+            "../Tatto.Pet/Богдан/photo_4.jpg",
+            "../Tatto.Pet/Богдан/photo_5.jpg",
+            "../Tatto.Pet/Богдан/photo_6.jpg",
+            "../Tatto.Pet/Богдан/photo_7.jpg",
+            "../Tatto.Pet/Богдан/photo_8.jpg",
+            "../Tatto.Pet/Богдан/photo_9.jpg",
+            "../Tatto.Pet/Богдан/photo_10.jpg",
+            "../Tatto.Pet/Богдан/photo_11.jpg",
+            "../Tatto.Pet/Богдан/photo_12.jpg",
+            "../Tatto.Pet/Богдан/photo_13.jpg",
+            "../Tatto.Pet/Богдан/photo_14.jpg",
+            "../Tatto.Pet/Богдан/photo_15.jpg",
+            "../Tatto.Pet/Богдан/photo_16.jpg",
+        ]
+    },
+    {
+        id: "denis",
+        name: "Денис",
+        works: [
+            "../Tatto.Pet/Денис/photo_1.jpg",
+            "../Tatto.Pet/Денис/photo_2.jpg",
+            "../Tatto.Pet/Денис/photo_3.jpg",
+            "../Tatto.Pet/Денис/photo_4.jpg",
+            "../Tatto.Pet/Денис/photo_5.jpg",
+            "../Tatto.Pet/Денис/photo_6.jpg",
+            "../Tatto.Pet/Денис/photo_7.jpg",
+            "../Tatto.Pet/Денис/photo_8.jpg",
+            "../Tatto.Pet/Денис/photo_9.jpg",
+            "../Tatto.Pet/Денис/photo_10.jpg",
+            "../Tatto.Pet/Денис/photo_11.jpg",
+            "../Tatto.Pet/Денис/photo_12.jpg",
+            "../Tatto.Pet/Денис/photo_13.jpg",
+            "../Tatto.Pet/Денис/photo_14.jpg",
+            "../Tatto.Pet/Денис/photo_15.jpg",
+            "../Tatto.Pet/Денис/photo_16.jpg",
+        ]
+    },
+    {
+        id: "andrey",
+        name: "Андрей",
+        works: [
+            "../Tatto.Pet/Андрей/photo_1.jpg",
+            "../Tatto.Pet/Андрей/photo_2.jpg",
+            "../Tatto.Pet/Андрей/photo_3.jpg",
+            "../Tatto.Pet/Андрей/photo_4.jpg",
+            "../Tatto.Pet/Андрей/photo_5.jpg",
+            "../Tatto.Pet/Андрей/photo_6.jpg",
+            "../Tatto.Pet/Андрей/photo_7.jpg",
+            "../Tatto.Pet/Андрей/photo_8.jpg",
+        ]
+    },
+    {
+        id: "olga",
+        name: "Ольга",
+        works: [
+            "../Tatto.Pet/Ольга/photo_1.jpg",
+            "../Tatto.Pet/Ольга/photo_2.jpg",
+            "../Tatto.Pet/Ольга/photo_3.jpg",
+            "../Tatto.Pet/Ольга/photo_4.jpg",
+            "../Tatto.Pet/Ольга/photo_5.jpg",
+            "../Tatto.Pet/Ольга/photo_6.jpg",
+            "../Tatto.Pet/Ольга/photo_7.jpg",
+            "../Tatto.Pet/Ольга/photo_8.jpg",
+        ]
+    },
+    {
+        id: "dmitriy",
+        name: "Дмитрий",
+        works: [
+            "../Tatto.Pet/Дмитрий/photo_1.jpg",
+            "../Tatto.Pet/Дмитрий/photo_2.jpg",
+            "../Tatto.Pet/Дмитрий/photo_3.jpg",
+            "../Tatto.Pet/Дмитрий/photo_4.jpg",
+            "../Tatto.Pet/Дмитрий/photo_5.jpg",
+        ]
+    },
+    {
+        id: "alina",
+        name: "Алина",
+        works: [
+            "../Tatto.Pet/Алина/photo_1.jpg",
+            "../Tatto.Pet/Алина/photo_2.jpg",
+            "../Tatto.Pet/Алина/photo_3.jpg",
+            "../Tatto.Pet/Алина/photo_4.jpg",
+            "../Tatto.Pet/Алина/photo_5.jpg",
+            "../Tatto.Pet/Алина/photo_6.jpg",
+            "../Tatto.Pet/Алина/photo_7.jpg",
+            "../Tatto.Pet/Алина/photo_8.jpg",
+            "../Tatto.Pet/Алина/photo_9.jpg",
+            "../Tatto.Pet/Алина/photo_10.jpg",
+            "../Tatto.Pet/Алина/photo_11.jpg",
+            "../Tatto.Pet/Алина/photo_12.jpg",
+            "../Tatto.Pet/Алина/photo_13.jpg",
+            "../Tatto.Pet/Алина/photo_14.jpg",
+            "../Tatto.Pet/Алина/photo_15.jpg",
+            "../Tatto.Pet/Алина/photo_16.jpg",
+            "../Tatto.Pet/Алина/photo_17.jpg",
+            "../Tatto.Pet/Алина/photo_18.jpg",
+            "../Tatto.Pet/Алина/photo_19.jpg",
+            "../Tatto.Pet/Алина/photo_20.jpg",
+            "../Tatto.Pet/Алина/photo_21.jpg",
+            "../Tatto.Pet/Алина/photo_22.jpg",
+            "../Tatto.Pet/Алина/photo_23.jpg",
+        ]
+    }
+];
+
+
+
+
+const galleryImages = [
+  "../Tatto.Pet/Galary/photo_1.jpg",
+  "../Tatto.Pet/Galary/photo_2.jpg",
+  "../Tatto.Pet/Galary/photo_3.jpg",
+  "../Tatto.Pet/Galary/photo_4.jpg",
+  "../Tatto.Pet/Galary/photo_5.jpg",
+  "../Tatto.Pet/Galary/photo_6.jpg",
+  "../Tatto.Pet/Galary/photo_7.jpg",
+  "../Tatto.Pet/Galary/photo_8.jpg",
+  "../Tatto.Pet/Galary/photo_9.jpg",
+  "../Tatto.Pet/Galary/photo_10.jpg",
+  "../Tatto.Pet/Galary/photo_11.jpg",
+  "../Tatto.Pet/Galary/photo_12.jpg",
+  "../Tatto.Pet/Galary/photo_14.jpg",
+  "../Tatto.Pet/Galary/photo_15.jpg",
+  "../Tatto.Pet/Galary/photo_16.jpg",
+  "../Tatto.Pet/Galary/photo_17.jpg",
+  "../Tatto.Pet/Galary/photo_18.jpg",
+  "../Tatto.Pet/Galary/photo_19.jpg",
+  "../Tatto.Pet/Galary/photo_20.jpg",
+  "../Tatto.Pet/Galary/photo_21.jpg",
+  "../Tatto.Pet/Galary/photo_22.jpg",
+  "../Tatto.Pet/Galary/photo_23.jpg",
+  "../Tatto.Pet/Galary/photo_24.jpg",
+  "../Tatto.Pet/Galary/photo_25.jpg",
+  "../Tatto.Pet/Galary/photo_26.jpg",
+  "../Tatto.Pet/Galary/photo_27.jpg",
+  "../Tatto.Pet/Galary/photo_28.jpg",
+  "../Tatto.Pet/Galary/photo_29.jpg",
+  "../Tatto.Pet/Galary/photo_30.jpg",
+  "../Tatto.Pet/Galary/photo_31.jpg",
+];
+
+
+const aboutImg = document.querySelector(".about__img");
+const aboutImages = [
+  "../Tatto.Pet/About/photo_1.jpg",
+  "../Tatto.Pet/About/photo_2.jpg",
+  "../Tatto.Pet/About/photo_3.jpg",
+  "../Tatto.Pet/About/photo_4.jpg",
+  "../Tatto.Pet/About/photo_5.jpg",
+  "../Tatto.Pet/About/photo_6.jpg",
+  "../Tatto.Pet/About/photo_7.jpg",
+  "../Tatto.Pet/About/photo_8.jpg",
+  "../Tatto.Pet/About/photo_9.jpg",
+  "../Tatto.Pet/About/photo_10.jpg",
+  "../Tatto.Pet/About/photo_11.jpg",
+];
+
+
+const modal = document.getElementById("modal");
+const gallery = document.getElementById("modalGallery");
+
+// клик по карточке
+document.querySelectorAll(".masters__card").forEach(card => {
+  card.addEventListener("click", () => {
+    const id = card.dataset.master;
+    const master = masters.find(m => m.id === id);
+    if (!master) return; // защита
+    gallery.innerHTML = "";
+    gallery.classList.remove("single");
+    master.works.forEach(src => {
+      const img = document.createElement("img");
+      img.src = src;
+      gallery.appendChild(img);
+    });
+
+    // открываю модалку
+    modal.classList.add("active");
+    document.body.style.overflow = "hidden"
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("active");
+document.body.style.overflow = "";
+  }
+});
+
+
+
+gallery.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    const src = e.target.src;
+
+    gallery.innerHTML = `
+      <img src="${src}" class="modal__big-img">
+    `;
+    gallery.classList.add("single");
+  }
+});
+
+
+
+document.querySelectorAll(".galary__img").forEach(img => {
+  img.addEventListener("click", () => {
+    gallery.innerHTML = "";
+
+    const bigImg = document.createElement("img");
+    bigImg.src = img.src;
+    bigImg.classList.add("modal__big-img");
+
+    gallery.appendChild(bigImg);
+
+    modal.classList.add("active");
+    document.body.style.overflow = "hidden";
+  });
+});
+
+
+
+// для галереи
+document.querySelector(".galary__btn").addEventListener("click", () => {
+  gallery.innerHTML = "";
+
+  gallery.classList.remove("single"); //если был большой режим
+
+  galleryImages.forEach(src => {
+    const img = document.createElement("img");
+    img.src = src;
+    gallery.appendChild(img);
+  });
+
+  modal.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+document.querySelectorAll(".galary__img").forEach(img => {
+  img.addEventListener("click", () => {
+    gallery.innerHTML = "";
+
+    gallery.classList.add("single"); //режим одной картинки
+
+    const bigImg = document.createElement("img");
+    bigImg.src = img.src;
+    bigImg.classList.add("modal__big-img");
+
+    gallery.appendChild(bigImg);
+
+    modal.classList.add("active");
+    document.body.style.overflow = "hidden";
+  });
+});
+
+
+//about
+
+let current = 0;
+aboutImg.src = aboutImages[current];
+setInterval(() => {
+  aboutImg.classList.add("fade");
+
+  setTimeout(() => {
+    current = (current + 1) % aboutImages.length;
+    aboutImg.src = aboutImages[current];
+
+    aboutImg.classList.remove("fade");
+  }, 500);
+
+}, 3000);
+
+
+
+// ==========================================
+// Основная модалка
+// ==========================================
+
+const burgerBtn = document.querySelector('.header__burger');
+const burgerMenu = document.getElementById('burgerMenu');
+const burgerOverlay = document.getElementById('burgerOverlay');
+const burgerClose = document.getElementById('burgerClose');
+const menuLinks = document.querySelectorAll('.burger-menu__link');
+const galleryModal = document.querySelector('.modal')
+// Функция для открытия меню
+function openMenu() {
+    // принудительно закрываю модалку
+    if (galleryModal && galleryModal.classList.contains('active')) {
+        galleryModal.classList.remove('active');
+    }
+
+    burgerMenu.classList.add('active');
+    burgerOverlay.classList.add('active');
+    document.body.style.overflow = 'hidden'; 
+}
+
+// Функция для закрытия меню
+function closeMenu() {
+    burgerMenu.classList.remove('active');
+    burgerOverlay.classList.remove('active');
+    document.body.style.overflow = ''; /* Возвращаем скролл сайту */
+}
+
+// события клика
+if (burgerBtn) burgerBtn.addEventListener('click', openMenu);
+if (burgerClose) burgerClose.addEventListener('click', closeMenu);
+if (burgerOverlay) burgerOverlay.addEventListener('click', closeMenu);
+
+// Закрываю меню при клике на любую ссылку чтобы переместиться к секции
+menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+});
+
